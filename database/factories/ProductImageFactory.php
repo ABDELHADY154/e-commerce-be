6dbcc6f7d259a70f3de5db28f3d3735cd5ce8a93
Mohamed Abdelhady Elshamy <mohamedabdelhady88@gmail.com>
2021-08-19 16:38:42 +0200

    .

--- database/factories/ProductImageFactory.php
+++ database/factories/ProductImageFactory.php
@@ -7,9 +7,9 @@ use App\ProductImage;
 use Faker\Generator as Faker;
 
 $factory->define(ProductImage::class, function (Faker $faker) {
-    $images = ['p1.jpeg', 'p2.jpeg', 'p3.jpeg'];
+    $images = ['p1.jpeg', 'p2.jpeg', 'p3.jpeg', 'p4.jpeg'];
     return [
-        'image' => $images[rand(0, 2)],
+        'image' => $images[rand(0, 3)],
         'product_id' => rand(1, Product::all()->count()),
     ];
 });
