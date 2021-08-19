da5114e1131a60fe4a8e6ad2e94754dd0291c80d
Mohamed Abdelhady Elshamy <mohamedabdelhady88@gmail.com>
2021-08-19 15:39:23 +0200

    .

--- app/Http/Resources/API/ProductResoource.php
+++ app/Http/Resources/API/ProductResoource.php
@@ -15,6 +15,7 @@ class ProductResoource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'brand' => $this->category->brand->brand,
             'price' => $this->price,
