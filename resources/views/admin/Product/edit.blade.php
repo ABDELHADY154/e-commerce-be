02bc84b5c931fb0f62efa2012951ddc86a953ebf
Mohamed Abdelhady Elshamy <mohamedabdelhady88@gmail.com>
2021-08-21 01:24:27 +0200

    .

--- resources/views/admin/Product/edit.blade.php
+++ resources/views/admin/Product/edit.blade.php
@@ -10,8 +10,9 @@
 
 <div class="container">
     <div class="card p-3">
-        {{-- @livewire('product-edit-form', ['model' => $product]) --}}
-        <livewire:product-edit-form :model="{{$product}}" />
+        @livewire('product-edit-form', ['model' => $product])
+
+        {{-- <livewire:product-edit-form :model="{{$product}}" /> --}}
     </div>
 </div>
 
