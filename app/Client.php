28a2c3690e416fe352ea17bf03018477686cfdec
Mohamed Abdelhady Elshamy <mohamedabdelhady88@gmail.com>
2021-09-13 01:39:57 +0200

    ..

--- app/Client.php
+++ app/Client.php
@@ -7,11 +7,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Passport\HasApiTokens;
 use Overtrue\LaravelFavorite\Traits\Favoriter;
-use Relative\LaravelExpoPushNotifications\Traits\HasPushTokens;
 
 class Client extends Authenticatable
 {
-    use Favoriter, SoftDeletes, HasApiTokens, HasPushTokens;
+    use Favoriter, SoftDeletes, HasApiTokens;
 
     protected $fillable = [
         'name', 'email', 'password', 'phone_number', 'image'
