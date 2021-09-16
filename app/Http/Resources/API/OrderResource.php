9e83b2b89f7e898b6188f872a78e083a51581ecb
Mohamed Abdelhady Elshamy <mohamedabdelhady88@gmail.com>
2021-09-16 15:36:03 +0200

    .

--- app/Http/Resources/API/OrderResource.php
+++ app/Http/Resources/API/OrderResource.php
@@ -14,11 +14,16 @@ class OrderResource extends JsonResource
      */
     public function toArray($request)
     {
+        $quantity = 0;
+        foreach ($this->products as $product) {
+            // dd();
+            $quantity += $product->pivot->quantity;
+        }
         return [
             'id' => $this->id,
             'order_num' => $this->order_num,
             'status' => $this->status,
-            'quantity' => count($this->products),
+            'quantity' => $quantity,
             'created_at' => date("Y-m-d", $this->created_at->timestamp),
             'price' => $this->price,
             'delivery' => $this->delivery,
