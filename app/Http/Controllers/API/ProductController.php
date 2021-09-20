54c31c04b65da2c3e96336c568f115ab900ef0e4
Mohamed Abdelhady Elshamy <mohamedabdelhady88@gmail.com>
2021-09-20 21:21:46 +0200

    .

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -85,13 +85,13 @@ class ProductController extends Controller
 
     public function getLatestNewProducts()
     {
-        $products = Product::where('sale', false)->orderBy('id', 'desc')->take(10)->get();
+        $products = Product::where('sale', false)->orderBy('id', 'desc')->take(10)->inRandomOrder()->get();
         return $this->ok(ProductResoource::collection($products)->resolve());
     }
 
     public function getLatestSaleProducts()
     {
-        $products = Product::where('sale', true)->orderBy('id', 'desc')->take(10)->get();
+        $products = Product::where('sale', true)->orderBy('id', 'desc')->take(10)->inRandomOrder()->get();
         return $this->ok(ProductResoource::collection($products)->resolve());
     }
 }
